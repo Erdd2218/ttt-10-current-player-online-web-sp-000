4fb14240a7989eee7262438ed1e1e538622f62b2
Andre Pierre <andre.laurence94@gmail.com>
2021-05-02 03:04:22 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,4 +1,32 @@
-counter = 0
- if my_condition_is_met
- 	counter = counter + 1
+def turn_count(board)
+   # count the number of positions that are occupied on the board (i.e. in the board array)
+   #  by iterating over the board array and incrementing a count variable if a position is filled 
+   #  with an X or an O
+   i = 0
+   count = 0
+   while i <= 9
+     if board[i] == "X" || board[i] == "O"
+       count += 1
+     end
+     i += 1
+   end
+   count
+   #     turns += 1
+   #   end
+   # end
+   # turns = 0
+   # board.each do |space|
+   #   if space == "X" || space == "O"
+   #     turns += 1
+   #   end
+   # end
+   # turns
+
  end
+
+ turn_count(["O", " ", " ", " ", "X", " ", " ", " ", "X"])
+
+ def current_player(board)
+   # first person to go is always X, so if turn count is even, then it is X turn, if turn count is odd, O turn
+   turn_count(board).even? ? "X" : "O"
+ end 
